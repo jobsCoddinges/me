8bf19d0b5cd074bb59cb2d40028ef28988fb41fc
jobsCoddinges <tlsrbcjf79@gmail.com>
2022-09-28 23:34:55 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.scss";
 import styled from "styled-components";
 import { AnimatePresence, motion } from "framer-motion";
 import blue from "./img/blue.png";
-import { usePresence } from "framer-motion";
+
 import Intro from "./components/Intro";
 
 const Wrapper = styled.div`
@@ -50,14 +50,22 @@ const pathVariants = {
 function App() {
   const [visible, setVisible] = useState(true);
   const [visibleTwo, setVisibleTwo] = useState(false);
-  const Refs = useRef<SVGPathElement>(null);
-
-  const changeDetect = setInterval(() => {
-    const value = Refs.current?.attributes[3].value;
+  const helloRef = useRef<SVGPathElement>(null);
+  const iamRef = useRef<SVGPathElement>(null);
+  const helloChangeDetect = setInterval(() => {
+    const value = helloRef.current?.attributes[3].value;
     if (value === "rgba(255,255,255,0)") return;
     else {
       setVisible(false);
-      clearInterval(changeDetect);
+      clearInterval(helloChangeDetect);
+    }
+  }, 2000);
+  const iamChangeDetect = setInterval(() => {
+    const value = iamRef.current?.attributes[3].value;
+    if (value === "rgba(255,255,255,0)") return;
+    else {
+      setVisibleTwo(false);
+      clearInterval(iamChangeDetect);
     }
   }, 2000);
 
@@ -68,7 +76,7 @@ function App() {
           <SvgWrapper>
             <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
               <motion.path
-                ref={Refs}
+                ref={helloRef}
                 variants={pathVariants}
                 initial="hidden"
                 animate="visible"
@@ -88,7 +96,18 @@ function App() {
       <AnimatePresence>
         {visibleTwo && (
           <SvgWrapper>
-            <Intro dContent="M32 32C14.3 32 0 46.3 0 64S14.3 96 32 96h96V416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H288c17.7 0 32-14.3 32-32s-14.3-32-32-32H192V96h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H160 32z" />
+            <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+              <motion.path
+                ref={iamRef}
+                variants={pathVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                strokeWidth={3}
+                stroke="white"
+                d="M32 32C14.3 32 0 46.3 0 64S14.3 96 32 96h96V416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H288c17.7 0 32-14.3 32-32s-14.3-32-32-32H192V96h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H160 32z"
+              />
+            </Svg>
             <Intro
               style={{ marginLeft: "20px" }}
               dContent="M253.5 51.7C248.6 39.8 236.9 32 224 32s-24.6 7.8-29.5 19.7l-120 288-40 96c-6.8 16.3 .9 35 17.2 41.8s35-.9 41.8-17.2L125.3 384H322.7l31.8 76.3c6.8 16.3 25.5 24 41.8 17.2s24-25.5 17.2-41.8l-40-96-120-288zM296 320H152l72-172.8L296 320z"
